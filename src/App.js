e3eda3739df9990013479ad70bd08bcfe1d7a842
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-08-27 10:57:08 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormularioCadastro from './components/FormularioCadastro'
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Formulário de Cadastro</h1>
+      <FormularioCadastro />
+    </>
+  );
+}
+
+export default App;
